486ade7bf9763eb4cfc97f3322675327627186a7
raemundo <eimooamai@outlook.com>
2021-11-14 23:51:34 +0200

    feat: imporve OKHeader

--- src/components/okuna-logo/OkLogo.tsx
+++ src/components/okuna-logo/OkLogo.tsx
@@ -5,9 +5,6 @@ import logo from "./assets/logo.png";
 
 export default function OkLogo() {
     return (
-        <View style={t`flex-row`}>
-            <Image source={logo} style={t`h-10 w-10`} />
-            <Text style={t`font-bold text-2xl`}>Okuna</Text>
-        </View>
+        <Image source={logo} style={t`h-10 w-10`} />
     )
 }
