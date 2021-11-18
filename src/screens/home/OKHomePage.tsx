a4896a92cdf4abeb6446b29d58873fcc606d26e2
raemundo <eimooamai@outlook.com>
2021-11-18 17:26:34 +0200

    feat: reimplement OKHomePage

--- src/screens/home/OKHomePage.tsx
+++ src/screens/home/OKHomePage.tsx
@@ -1,22 +1,16 @@
 import React from "react";
 import OKHeader from "./components/header/OkHeader";
-import OKCreatePostBox from "./OKCreatePostBox";
-import OKProfileScreen from "./OkProfileScreen";
-import OKPost from "./OKPost";
-import { View, Text, Image, TextInput, Pressable } from 'react-native'
-import tw, { t } from "~/lib/tailwind"
+import {
+    View,
+} from "react-native";
+import { t } from "~/lib/tailwind";
 function OKHomePage({ children }) {
     return (
         <>
             <OKHeader />
-            {/* <View style={t`bg-gray-100 h-[1500px] items-center`}>
-                <View style={t`w-6/12 bg-gray-100`}>
-                    <OKCreatePostBox />
-                    <OKPost />
-                </View>
-            </View> */}
-            <OKProfileScreen />
-            {children}
+            <View style={t`bg-gray-50`}>
+                {children}
+            </View>
         </>
     )
 }
