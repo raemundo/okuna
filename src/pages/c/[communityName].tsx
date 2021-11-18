c7038aabd1879ba10b5da48f17641e393e542578
raemundo <eimooamai@outlook.com>
2021-11-18 17:27:55 +0200

    feat: rewrite communityName

--- src/pages/c/[communityName].tsx
+++ src/pages/c/[communityName].tsx
@@ -1,3 +1,10 @@
 import OKHomePage from "~/screens/home/OKHomePage";
+import OkDesktopCommunityProfile from "~/screens/home/pages/community/OkCommunityProfile";
 
-export default OKHomePage;
+export default function CommunityName() {
+    return (
+        <OKHomePage>
+            <OkDesktopCommunityProfile />
+        </OKHomePage>
+    )
+};
