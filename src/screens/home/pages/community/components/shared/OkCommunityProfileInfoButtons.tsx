2701089b4fd079986697f9acfc2295fd68149d3a
raemundo <eimooamai@outlook.com>
2021-11-18 21:48:09 +0200

    fix: OkCommunityProfileInfoButtons style

--- src/screens/home/pages/community/components/shared/OkCommunityProfileInfoButtons.tsx
+++ src/screens/home/pages/community/components/shared/OkCommunityProfileInfoButtons.tsx
@@ -7,8 +7,8 @@ import { faSmile, faBook } from "@fortawesome/free-solid-svg-icons";
 export default function OkCommunityProfileInfoButtons() {
     return (
         <View style={t`flex-row border-t border-gray-400 mt-5`}>
-            <View style={t`flex-row m-2 p-1 bg-gray-200 rounded-lg`}><FontAwesomeIcon style={tw`text-gray-600`} icon={faSmile} /> <Text style={t`text-gray-600`}>Staff</Text></View>
-            <View style={t`flex-row m-2 p-1 bg-gray-200 rounded-lg`}><FontAwesomeIcon style={tw`text-gray-600`} icon={faBook} />  <Text style={t`text-gray-600`}>Rules</Text></View>
+            <View style={t`flex-row m-2 p-1 items-center bg-gray-200 rounded-lg`}><FontAwesomeIcon style={tw`mx-1 text-gray-600`} icon={faSmile} /> <Text style={t`text-gray-600`}>Staff</Text></View>
+            <View style={t`flex-row m-2 p-1 items-center bg-gray-200 rounded-lg`}><FontAwesomeIcon style={tw`mx-1 text-gray-600`} icon={faBook} />  <Text style={t`text-gray-600`}>Rules</Text></View>
         </View>
     )
 }
