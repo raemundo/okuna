3ad3b9682d0429a6da3dd4d9c56fa2a3f506085c
raemundo <eimooamai@outlook.com>
2021-11-20 19:01:17 +0200

    feat: add sass

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import App from "next/app";
 import { useRouter } from "next/router";
+import "~/assets/styles/index.scss"
 import tw, { useDeviceContext } from "~/lib/tailwind";
 
 function MyApp({ Component, pageProps }) {
