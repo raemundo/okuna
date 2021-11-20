4e237859c2f399abdea3ee8f934463baf45dd180
raemundo <eimooamai@outlook.com>
2021-11-20 19:00:53 +0200

    build: support sass in style-jsx

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,26 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      "@expo/next-adapter/babel",
+    "presets": [
+      [
+        "@expo/next-adapter/babel",
+        {
+          "styled-jsx": {
+            "plugins": [
+              "@styled-jsx/plugin-sass"
+            ]
+          }
+        }
+      ]
     ],
 
     "plugins": [
-      ['@babel/plugin-proposal-class-properties'],
+      ["@babel/plugin-proposal-class-properties", { "loose": false }],
       ["module-resolver", {
         root: ["."],
         alias: {
-         "@": "./",
-         "~": "./src",
+          "@": "./",
+          "~": "./src",
         }
       }]
     ],
