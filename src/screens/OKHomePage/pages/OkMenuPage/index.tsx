0ae6e1db8f57ab934a85dab56f362030613289b5
raemundo <eimooamai@outlook.com>
2021-11-24 21:08:51 +0200

    refactor: close #2

---
+++ src/screens/OKHomePage/pages/OkMenuPage/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function OkMenuPage() {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
