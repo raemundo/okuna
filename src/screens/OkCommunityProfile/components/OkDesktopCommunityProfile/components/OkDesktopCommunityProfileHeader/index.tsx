96953e52af78dc993ab153efc93a282335e35eb0
raemundo <eimooamai@outlook.com>
2021-11-25 13:20:43 +0200

    refactor(components): OkDesktopCommunityProfileHeader

--- src/screens/OkCommunityProfile/components/OkDesktopCommunityProfile/components/OkDesktopCommunityProfileHeader/index.tsx
+++ src/screens/OkCommunityProfile/components/OkDesktopCommunityProfile/components/OkDesktopCommunityProfileHeader/index.tsx
@@ -1,58 +1,19 @@
 import React from 'react'
-import { View, Text, ImageBackground, Pressable } from 'react-native'
+import { View } from 'react-native'
 import { tw } from "~/lib/tailwind";
-import FontAwesomeIcon from "~/components/FontAwesomeIcon";
-import { faEllipsisV, faCoffee } from "@fortawesome/free-solid-svg-icons";
+
+
+import OkCommunityCover from "~/components/covers/OkCommunityCover";
+import OkDesktopCommunityProfileHeaderBar from '~/screens/OkCommunityProfile/components/OkDesktopCommunityProfile/components/OkDesktopCommunityProfileHeader/components/OkDesktopCommunityProfileHeaderBar'
+import OkCommunityAvatar from "~/components/avatars/OkCommunityAvatar";
 
 export default function OkDesktopCommunityProfileHeader() {
     return (
         <View
             style={tw`w-full shadow border border-gray-300 bg-white rounded-lg`}
         >
-            <ImageBackground
-                source={{ uri: "https://picsum.photos/720" }}
-                imageStyle={tw`rounded-t-2`}
-                style={tw`h-96 w-full rounded-t-4`}
-            ></ImageBackground>
-            <View style={tw`w-auto flex-row flex-wrap items-center`}>
-                <View style={tw` flex-row justify-start mb-2 w-9/12`}>
-                    <View style={tw`flex-row items-center mx-2`}>
-                        <FontAwesomeIcon
-                            icon={faCoffee}
-                            size="2x"
-                            style={{ paddingRight: 10 }}
-                        />
-                        <View>
-                            <Text style={tw`font-bold`}>Coffe</Text>
-                            <Text>c/coffee</Text>
-                        </View>
-                    </View>
-                    <View style={tw`hidden md:flex mx-2`}>
-                        <Text>Addicts</Text>
-                        <Text style={tw`font-bold`}>210</Text>
-                    </View>
-                    <View style={tw`hidden md:flex mx-2`}>
-                        <Text>Addicts</Text>
-                        <Text style={tw`font-bold`}>210</Text>
-                    </View>
-                </View>
-
-                <View style={tw`flex-row items-center justify-end my-6 w-3/12`}>
-                    <Pressable
-                        style={tw`px-4 py-2 border border-transparent rounded-md flex-row rounded-20 bg-blue-800 text-white font-semibold focus:outline-none`}
-                    >
-                        Leave
-                    </Pressable>
-                    <Pressable
-                        style={(state) => [
-                            tw`px-3 py-1.5 flex-row rounded-md font-semibold focus:outline-none`,
-                            state.hovered && tw`bg-gray-200 rounded-full`,
-                        ]}
-                    >
-                        <FontAwesomeIcon icon={faEllipsisV} />
-                    </Pressable>
-                </View>
-            </View>
+            <OkCommunityCover />
+            <OkDesktopCommunityProfileHeaderBar />
         </View>
     )
 }
