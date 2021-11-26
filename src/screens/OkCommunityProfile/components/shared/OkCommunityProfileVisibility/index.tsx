e2f6a8d195bc9e5e97cbf06f96693199c234bae7
raemundo <eimooamai@outlook.com>
2021-11-26 16:43:22 +0200

    fix(components/OkCommunityProfileVisibility): icon size

--- src/screens/OkCommunityProfile/components/shared/OkCommunityProfileVisibility/index.tsx
+++ src/screens/OkCommunityProfile/components/shared/OkCommunityProfileVisibility/index.tsx
@@ -5,8 +5,8 @@ import { faGlobeAmericas } from "@fortawesome/free-solid-svg-icons";
 import twi, { tw } from "~/lib/tailwind";
 export default function OkCommunityProfileVisibility() {
     return (
-        <View style={tw`flex-row mt-5`}>
-            <FontAwesomeIcon icon={faGlobeAmericas} style={twi`px-2 text-gray-500`} /> <Text style={tw`text-gray-500`}>Public</Text>
+        <View style={tw`flex-row items-center mt-5`}>
+            <FontAwesomeIcon icon={faGlobeAmericas} size="2x" style={twi`px-2 text-gray-500`} /> <Text style={tw`text-gray-500`}>Public</Text>
         </View>
     )
 }
