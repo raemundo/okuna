c1249dd1d08ba8fcc0cdd371b9fae2297523438f
raemundo <eimooamai@outlook.com>
2021-11-26 08:33:15 +0200

    refactor(components): OkModals

---
+++ src/layouts/OKHomePage/components/OkModals/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function OkModals() {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
