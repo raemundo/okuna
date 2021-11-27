0b1c8b648f019f55bb550c9dffdd35f32a396be8
raemundo <eimooamai@outlook.com>
2021-11-27 08:26:11 +0200

    fix(style): Logo Image

--- src/components/OkLogo/index.tsx
+++ src/components/OkLogo/index.tsx
@@ -6,8 +6,8 @@ import logo from "./assets/logo.png";
 export default function OkLogo({style}) {
     const getStyle = style ?? {}
     return (
-        <View style={[tw`flex-row items-center justify-center`, getStyle]}>
-            <Image source={logo} style={tw`h-10 w-10`} />
+        <View style={[tw`flex-row items-center justify-center p-2.5`, getStyle]}>
+            <Image source={logo} style={tw`h-4 w-4 md:h-10 md:w-10`} />
             <Text style={tw`text-xl font-semibold hidden md:flex`}>Okuna</Text>
         </View>
     )
