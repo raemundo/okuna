a32baa847a9e087a6cee7a473fa73be862ad42ff
raemundo <eimooamai@outlook.com>
2021-11-28 20:30:19 +0200

    fix(OkLogo): style

--- src/components/OkLogo/index.tsx
+++ src/components/OkLogo/index.tsx
@@ -6,9 +6,9 @@ import logo from "./assets/logo.png";
 export default function OkLogo({style}) {
     const getStyle = style ?? {}
     return (
-        <View style={[tw`flex-row items-center justify-center p-2.5`, getStyle]}>
-            <Image source={logo} style={tw`h-4 w-4 md:h-10 md:w-10`} />
-            <Text style={tw`text-xl font-semibold hidden md:flex`}>Okuna</Text>
+        <View style={[tw`flex-row items-center p-2.5`, getStyle]}>
+            <Image source={logo} style={tw`w-4 h-4 md:h-10 md:w-10`} />
+            <Text style={tw`hidden text-xl font-semibold md:flex`}>Okuna</Text>
         </View>
     )
 }
