dcf77de33d4275e1a796a26fa5df2860f59cbc03
raemundo <eimooamai@outlook.com>
2021-11-29 19:46:38 +0200

    feat(OkUserMenuDropdown): reimplement

--- src/layouts/OkHomePage/components/OkHeader/components/OkUserMenuDropdown/index.tsx
+++ src/layouts/OkHomePage/components/OkHeader/components/OkUserMenuDropdown/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Pressable, Image } from 'react-native'
+import FontAwesomeIcon from "~/component-lib/FontAwesomeIcon";
+import { faSortDown } from "@fortawesome/free-solid-svg-icons";
 import twi, { tw } from "~/lib/tailwind";
+import OkUserAvatar from "~/components/avatars/OkUserAvatar";
+import Avatar from '~/component-lib/Avatar';
 
 export default function OkUserMenuDropdown() {
     return (
-        <View>
-            <Text></Text>
-        </View>
+        <Pressable style={(state) => [
+            tw`flex-row items-center justify-between px-2 py-1 bg-gray-200 rounded-full`,
+            state.hovered && tw`bg-gray-300`
+        ]}>
+            <Avatar source={{ uri: 'https://picsum.photos/200' }} style={tw`w-6 h-6`} />
+            <Text style={tw`hidden mx-4 text-sm font-semibold md:flex`}>Saiful</Text>
+            <FontAwesomeIcon icon={faSortDown} />
+        </Pressable >
     )
 }
