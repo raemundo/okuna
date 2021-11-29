2271122b09f4b75a6fe9daecabd19f88ac5e6ac7
raemundo <eimooamai@outlook.com>
2021-11-29 18:39:23 +0200

    feat: add style

--- src/component-lib/Card/index.tsx
+++ src/component-lib/Card/index.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import twi, { tw } from "~/lib/tailwind"
 
-export default function Card({ children }) {
+export default function Card({ children , style={}}) {
     return (
-        <View style={tw`w-full h-auto my-2 bg-white border border-gray-300 rounded-md shadow dark:border-gray-900 dark:bg-black`}>
+        <View style={[tw`w-full h-auto my-2 bg-white border border-gray-300 rounded-md shadow dark:border-gray-900 dark:bg-black`, style]}>
             {children}
         </View>
     )
