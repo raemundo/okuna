0e44dfec15a233834cea7189d5a73562e4564769
raemundo <eimooamai@outlook.com>
2021-11-29 17:04:43 +0200

    feat: add OkCategoryButton

--- src/components/buttons/OkCategoryButton/index.tsx
+++ src/components/buttons/OkCategoryButton/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Pressable } from 'react-native'
 import twi, { tw } from "~/lib/tailwind";
 
-export default function OkCategoryButton() {
+export default function OkCategoryButton({ children, color}) {
     return (
-        <View>
-            <Text></Text>
-        </View>
+        <Pressable style={(state) => [tw`px-2 py-1 mx-1 bg-${color}-500 rounded-xl text-white`]}>{children}</Pressable>
     )
 }
--- src/screens/OkCommunityProfile/components/shared/OkCommunityProfileCategories/index.tsx
+++ src/screens/OkCommunityProfile/components/shared/OkCommunityProfileCategories/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 import twi, { tw } from "~/lib/tailwind";
+import OkCategoryButton from "~/components/buttons/OkCategoryButton"
 
 export default function OkCommunityProfileCategories() {
     return (
         <View style={tw`flex-row`}>
-            <Text style={tw`bg-yellow-500 py-1 px-2 mx-1 rounded-xl`}>food</Text>
-            <Text style={tw`bg-yellow-500 py-1 px-2 mx-1 rounded-xl`}>food</Text>
-        </View>
+            <OkCategoryButton color="yellow">food</OkCategoryButton>
+            <OkCategoryButton color="green">photos</OkCategoryButton>
+        </View >
     )
 }
