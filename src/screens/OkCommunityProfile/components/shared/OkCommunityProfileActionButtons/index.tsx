c4f28562ecabc469683a89a455855698c964fc03
raemundo <eimooamai@outlook.com>
2021-12-01 18:08:23 +0200

    fix: text node directly in View component

--- src/screens/OkCommunityProfile/components/shared/OkCommunityProfileActionButtons/index.tsx
+++ src/screens/OkCommunityProfile/components/shared/OkCommunityProfileActionButtons/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import { View, Pressable } from 'react-native'
+import { View, Text, Pressable } from 'react-native'
 import twi, { tw } from "~/lib/tailwind";
 import FontAwesomeIcon from "~/component-lib/FontAwesomeIcon";
 import { faEllipsisV } from "@fortawesome/free-solid-svg-icons";
 
 export default function OkCommunityProfileActionButtons() {
     return (
-        <View style={tw`flex-row items-center justify-end my-6 w-3/12`}>
+        <View style={tw`flex-row items-center justify-end w-3/12 my-6`}>
             <Pressable
-                style={tw`px-4 py-2 border border-transparent rounded-md flex-row rounded-20 bg-blue-800 text-white font-semibold focus:outline-none`}
+                style={tw`flex-row px-4 py-2 font-semibold text-white bg-blue-800 border border-transparent rounded-md rounded-20 focus:outline-none`}
             >
-                Leave
+                <Text style={tw`font-bold text-white`}>Leave</Text>
             </Pressable>
             <Pressable
                 style={(state) => [
