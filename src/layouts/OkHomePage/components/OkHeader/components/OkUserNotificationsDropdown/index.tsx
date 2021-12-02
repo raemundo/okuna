9d12cb1a08440c3873df04ff68f0954c5d2cee95
raemundo <eimooamai@outlook.com>
2021-12-02 15:52:09 +0200

    feat(OkUserNotificationsDropdown): hide on press

--- src/layouts/OkHomePage/components/OkHeader/components/OkUserNotificationsDropdown/index.tsx
+++ src/layouts/OkHomePage/components/OkHeader/components/OkUserNotificationsDropdown/index.tsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, Pressable } from 'react-native'
 import twi, { tw } from "~/lib/tailwind";
 import OkUserNotifications from "~/components/OkUserNotifications";
 import FontAwesomeIcon from "~/component-lib/FontAwesomeIcon";
 import { faBell } from "@fortawesome/free-solid-svg-icons";
 
 export default function OkUserNotificationsDropdown() {
+    const [hidden, setHidden] = useState(true);
     return (
         <>
-            <FontAwesomeIcon style={twi`text-2xl text-black dark:text-white`} icon={faBell} />
-            <OkUserNotifications />
+            <Pressable onPress={() => { setHidden(!hidden) }}>
+                <FontAwesomeIcon style={twi`text-2xl text-black dark:text-white hidden lg:flex`} icon={faBell} />
+            </Pressable>
+            {!hidden && <OkUserNotifications />}
         </>
     )
 }
