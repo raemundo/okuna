dfa7e215ffba493413d7b1dc6aa2738a0cf9e0da
raemundo <eimooamai@outlook.com>
2021-12-08 19:12:13 +0200

    refactor(OkLoginPage): use Card

--- src/screens/OkLoginPage/index.tsx
+++ src/screens/OkLoginPage/index.tsx
@@ -4,18 +4,18 @@ import { tw } from "~/lib/tailwind"
 import OkLogo from "~/components/OkLogo";
 import OkLoginForm from "~/components/forms/OkLoginForm";
 import OkAuthPage from "~/layouts/OkAuthPage";
-
+import Card from "~/component-lib/Card"
 const t = (txt) => txt;
 function OkLoginPage() {
     return (
         <OkAuthPage>
-            <View style={tw`w-full bg-white border border-gray-100 rounded-lg md:w-1/2`}>
+            <Card style={tw`md:w-1/2`}>
                 <OkLogo style={tw`justify-center`} />
-                <View style={tw`p-2 border border-gray-100`}>
+                <View style={tw`p-2 dark:border-0 border border-gray-100`}>
                     <OkLoginForm />
                 </View>
                 <Text style={tw`px-6 text-lg text-center text-gray-600`}>{t('common:name')}</Text>
-            </View>
+            </Card>
         </OkAuthPage>
     )
 }
