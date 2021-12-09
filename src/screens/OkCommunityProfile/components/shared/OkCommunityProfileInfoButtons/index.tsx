fb4a40be8642eacc8762727ea2d1a6cbbe33d7cb
raemundo <eimooamai@outlook.com>
2021-12-09 15:22:39 +0200

    feat(OkCommunityProfileInfoButtons): add translation

--- src/screens/OkCommunityProfile/components/shared/OkCommunityProfileInfoButtons/index.tsx
+++ src/screens/OkCommunityProfile/components/shared/OkCommunityProfileInfoButtons/index.tsx
@@ -3,12 +3,15 @@ import { View, Text } from 'react-native'
 import twi, { tw } from "~/lib/tailwind";
 import FontAwesomeIcon from "~/component-lib/FontAwesomeIcon";
 import { faSmile, faBook } from "@fortawesome/free-solid-svg-icons";
+import { useTranslation } from 'react-i18next';
+
 
 export default function OkCommunityProfileInfoButtons() {
+    const { t } = useTranslation()
     return (
         <View style={tw`flex-row`}>
-            <View style={tw`flex-row items-center p-1 m-2 bg-gray-200 rounded-lg`}><Text style={tw`text-gray-600`}><FontAwesomeIcon style={twi`text-gray-600`} icon={faSmile} /> Staff</Text></View>
-            <View style={tw`flex-row items-center p-1 m-2 bg-gray-200 rounded-lg`}><Text style={tw`text-gray-600`}><FontAwesomeIcon style={twi`text-gray-600`} icon={faBook} /> Rules</Text></View>
+            <View style={tw`flex-row items-center p-1 m-2 bg-gray-200 rounded-lg`}><Text style={tw`text-gray-600`}><FontAwesomeIcon style={twi`text-gray-600`} icon={faSmile} /> {t('community__button_staff')}</Text></View>
+            <View style={tw`flex-row items-center p-1 m-2 bg-gray-200 rounded-lg`}><Text style={tw`text-gray-600`}><FontAwesomeIcon style={twi`text-gray-600`} icon={faBook} /> {t('community__button_rules')}</Text></View>
         </View>
     )
 }
