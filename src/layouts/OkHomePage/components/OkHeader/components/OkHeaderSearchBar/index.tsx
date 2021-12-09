83fe338922616ef55779b30c591ea3fcf3174327
raemundo <eimooamai@outlook.com>
2021-12-09 15:33:48 +0200

    feat(OkHeaderSearchBar): add translation

--- src/layouts/OkHomePage/components/OkHeader/components/OkHeaderSearchBar/index.tsx
+++ src/layouts/OkHomePage/components/OkHeader/components/OkHeaderSearchBar/index.tsx
@@ -3,11 +3,13 @@ import { TextInput, Pressable } from 'react-native'
 import FontAwesomeIcon from '~/component-lib/FontAwesomeIcon';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import twi, { tw } from '~/lib/tailwind'
+import { useTranslation } from 'react-i18next';
 
 export default function OkHeaderSearchBar() {
+    const { t } = useTranslation();
     return (
         <>
-            <TextInput style={tw`flex-1 hidden px-4 py-2 text-center bg-gray-100 border border-transparent dark:bg-gray-500 md:flex rounded-2xl`} placeholder="search" />
+            <TextInput style={tw`flex-1 hidden px-4 py-2 text-center bg-gray-100 border border-transparent dark:bg-gray-500 md:flex rounded-2xl`} placeholder={t('user_search__search_text')} />
         </>
     )
 }
