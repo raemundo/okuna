420f09ed96170458916e208e770a6d023e213392
raemundo <eimooamai@outlook.com>
2021-12-09 15:11:06 +0200

    feat(OkCommunityProfileVisibility): add translation

--- src/screens/OkCommunityProfile/components/shared/OkCommunityProfileVisibility/index.tsx
+++ src/screens/OkCommunityProfile/components/shared/OkCommunityProfileVisibility/index.tsx
@@ -3,11 +3,27 @@ import { View, Text } from 'react-native'
 import FontAwesomeIcon from "~/component-lib/FontAwesomeIcon";
 import { faGlobeAmericas } from "@fortawesome/free-solid-svg-icons";
 import twi, { tw } from "~/lib/tailwind";
+import { useTranslation } from 'react-i18next';
+
 export default function OkCommunityProfileVisibility() {
+    const { t } = useTranslation();
     return (
         <View style={tw`flex-row items-center mt-5`}>
-            <FontAwesomeIcon icon={faGlobeAmericas} size="2x" style={twi`px-2 text-gray-500`} />
-            <Text style={tw`text-gray-500 `}>Public</Text>
+            {true ?
+                (
+                    <>
+                        <FontAwesomeIcon icon={faGlobeAmericas} size="2x" style={twi`px-2 text-gray-500`} />
+                        <Text style={tw`text-gray-500 `}>{t('community__type_public')}</Text>)
+                    </>
+                )
+                :
+                (
+                    <>
+                        <FontAwesomeIcon icon={faGlobeAmericas} size="2x" style={twi`px-2 text-gray-500`} />
+                        <Text style={tw`text-gray-500 `}>{t('community__type_private')}</Text>)
+                    </>
+                )
+            }
         </View>
     )
 }
