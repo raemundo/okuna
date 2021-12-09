3e50ad72eedc5351bd6cbe638aaed77682b652db
raemundo <eimooamai@outlook.com>
2021-12-09 15:15:19 +0200

    feat(OkCommunityProfileActionButtons): add translation

--- src/screens/OkCommunityProfile/components/OkDesktopCommunityProfile/components/OkDesktopCommunityProfileSidebar/index.tsx
+++ src/screens/OkCommunityProfile/components/OkDesktopCommunityProfile/components/OkDesktopCommunityProfileSidebar/index.tsx
@@ -12,15 +12,17 @@ import OkJoinCommunityButton from '~/components/buttons/OkJoinCommunityButton';
 import OkCommunityProfileActionButtons from '~/screens/OkCommunityProfile/components/shared/OkCommunityProfileActionButtons';
 import OkMobileCommunityProfileTitle from '~/screens/OkCommunityProfile/components/OkMobileCommunityProfile/components/OkMobileCommunityProfileCard/components/OkMobileCommunityProfileTitle'
 import OkMobileCommunityProfileName from '~/screens/OkCommunityProfile/components/OkMobileCommunityProfile/components/OkMobileCommunityProfileCard/components/OkMobileCommunityProfileName';
-import OkSmartText from '~/components/smart-text/OkSmartText';
+import OkSmartText from '~/components/smart-text/OkSmartText'; 
+import { useTranslation } from 'react-i18next';
 
 export default function OkDesktopCommunityProfileSidebar() {
+    const { t } = useTranslation()
     return (
         <View style={tw`w-full mt-1 md:w-2/6`}>
             <Card
             >
                 <View style={tw`mb-2 `}>
-                    <Text style={tw`text-sm font-bold text-gray-400`}>About</Text>
+                    <Text style={tw`text-sm font-bold text-gray-400`}>{t('community__about')}</Text>
                 </View>
                 <View style={tw`flex p-2`}>
                     <OkSmartText
--- src/screens/OkCommunityProfile/components/shared/OkCommunityProfileActionButtons/index.tsx
+++ src/screens/OkCommunityProfile/components/shared/OkCommunityProfileActionButtons/index.tsx
@@ -3,15 +3,25 @@ import { View, Text, Pressable } from 'react-native'
 import twi, { tw } from "~/lib/tailwind";
 import FontAwesomeIcon from "~/component-lib/FontAwesomeIcon";
 import { faEllipsisV } from "@fortawesome/free-solid-svg-icons";
+import { useTranslation } from 'react-i18next';
 
 export default function OkCommunityProfileActionButtons() {
+    const { t } = useTranslation();
     return (
         <View style={tw`flex-row items-center justify-end w-3/12 my-6`}>
-            <Pressable
-                style={tw`flex-row px-4 py-2 font-semibold text-white bg-blue-800 border border-transparent rounded-md rounded-20 focus:outline-none`}
-            >
-                <Text style={tw`font-bold text-white`}>Leave</Text>
-            </Pressable>
+            {true ?
+                <Pressable
+                    style={tw`flex-row px-4 py-2 font-semibold text-white bg-blue-800 border border-transparent rounded-md rounded-20 focus:outline-none`}
+                >
+                    <Text style={tw`font-bold text-white`}> {t('community__join_community')}</Text>
+                </Pressable>
+                :
+                <Pressable
+                    style={tw`flex-row px-4 py-2 font-semibold text-white bg-blue-800 border border-transparent rounded-md rounded-20 focus:outline-none`}
+                >
+                    <Text style={tw`font-bold text-white`}> {t('community__leave_community')}</Text>
+                </Pressable>
+            }
             <Pressable
                 style={(state) => [
                     tw`px-3 py-1.5 flex-row rounded-md font-semibold focus:outline-none`,
