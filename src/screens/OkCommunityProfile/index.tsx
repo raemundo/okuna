763225ab4e7afb135afa7ecd233ef16e77e19bc2
raemundo <eimooamai@outlook.com>
2021-12-11 21:16:31 +0200

    feat(OkCommunityProfile): add OkMobileCommunityProfile

--- src/screens/OkCommunityProfile/index.tsx
+++ src/screens/OkCommunityProfile/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import OkDesktopCommunityProfile from "./components/OkDesktopCommunityProfile"
+import OkMobileCommunityProfile from "./components/OkMobileCommunityProfile"
 import OkHomePage from "~/layouts/OkHomePage";
+import twi from "~/lib/tailwind";
 
 export default function OkCommunityProfile() {
   return (
     <OkHomePage>
-      <OkDesktopCommunityProfile />
+      {twi.prefixMatch('md') && <OkDesktopCommunityProfile />}
+      {!twi.prefixMatch('md') && <OkMobileCommunityProfile />}  
     </OkHomePage>
   );
 };
