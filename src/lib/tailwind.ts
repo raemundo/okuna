b613ee527d00b28560e07f6acd002f915f479876
raemundo <eimooamai@outlook.com>
2021-12-11 12:38:50 +0200

    refactor: rm useDeviceContext hook

--- src/lib/tailwind.ts
+++ src/lib/tailwind.ts
@@ -17,25 +17,3 @@ tw.style = (...exprs) => StyleSheet.create({ x: twi.style(...exprs) }).x;
 // ... and then this becomes the main function your app uses
 export default twi;
 
-
-
-export function useDeviceContext(tw) {
-    const [isClient, setIsClient] = useState(false);
-    useEffect(() => {
-        // if (typeof window !== "undefined") {
-        // }
-        setIsClient(true);
-    }, []);
-    const window = useWindowDimensions();
-    const win = isClient ? window :
-        {
-            fontScale: 1,
-            height: 218,
-            scale: 1,
-            width: 1349,
-        }
-    tw.setWindowDimensions(win);
-    tw.setFontScale(win.fontScale);
-    tw.setPixelDensity(win.scale === 1 ? 1 : 2);
-    tw.setColorScheme(useColorScheme());
-}
