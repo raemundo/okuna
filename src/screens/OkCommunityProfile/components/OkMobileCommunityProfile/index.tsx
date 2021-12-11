58265923c038a706f52530f8f18584d96f93cb59
raemundo <eimooamai@outlook.com>
2021-12-11 21:15:19 +0200

    feat(OkMobileCommunityProfile): init

--- src/screens/OkCommunityProfile/components/OkMobileCommunityProfile/index.tsx
+++ src/screens/OkCommunityProfile/components/OkMobileCommunityProfile/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 import twi, { tw } from "~/lib/tailwind";
+import OkMobileCommunityProfileHeader from "./components/OkMobileCommunityProfileHeader";
+import OkMobileCommunityProfileCard from "./components/OkMobileCommunityProfileCard";
+import OkCommunityPostsStream from '~/components/posts-stream/OkCommunityPostsStream';
+import OkCommunityCover from '~/components/covers/OkCommunityCover';
+import OkNewPostAction from '~/components/OkNewPostAction';
 
 export default function OkMobileCommunityProfile() {
     return (
         <View>
-            <Text></Text>
+            <OkMobileCommunityProfileHeader />
+            <OkCommunityCover />
+            <OkMobileCommunityProfileCard />
+            <OkCommunityPostsStream />
+            <OkNewPostAction />
         </View>
     )
 }
