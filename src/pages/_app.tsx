bde65dde0fdc56e1e9775709cf7ddc2228dc7db0
raemundo <eimooamai@outlook.com>
2021-12-11 12:39:50 +0200

    feat: upgrade twrnc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import App from "next/app";
 import { useRouter } from "next/router";
 import "~/assets/styles/index.scss"
-import tw, { useDeviceContext } from "~/lib/tailwind";
+import tw from "~/lib/tailwind";
 import "~/lib/i18n";
+import { useDeviceContext, useAppColorScheme } from 'twrnc';
+import { TouchableOpacity, Text } from "react-native";
 
 function MyApp({ Component, pageProps }) {
-  useDeviceContext(tw);
+  const [isClient, setIsClient] = useState(false);
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  useDeviceContext(isClient ? tw : tw, { withDeviceColorScheme: false });
+  const [colorScheme, toggleColorScheme, setColorScheme] = useAppColorScheme(isClient ? tw : tw);
+
   const router = useRouter();
   return (
-  <>
-  <Component {...pageProps} key={router.asPath} />
-  <style jsx global> {`
+    <>
+      <TouchableOpacity onPress={toggleColorScheme}>
+        <Text style={tw`text-black dark:text-white`}>Switch Color Scheme {colorScheme} </Text>
+      </TouchableOpacity>
+      <Component {...pageProps} key={router.asPath} />
+      <style jsx global> {`
    body {  font-family: 'Nunito Sans'}
    `}</style>
-  </>
+    </>
   );
 }
 
