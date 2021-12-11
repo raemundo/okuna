ba03e482aca07ad15823dfd26c699bf65529a897
raemundo <eimooamai@outlook.com>
2021-12-11 21:16:58 +0200

    fix(OkDesktopCommunityProfileHeaderBarIds): style

--- src/screens/OkCommunityProfile/components/OkDesktopCommunityProfile/components/OkDesktopCommunityProfileHeader/components/OkDesktopCommunityProfileHeaderBar/components/OkDesktopCommunityProfileHeaderBarIds/index.tsx
+++ src/screens/OkCommunityProfile/components/OkDesktopCommunityProfile/components/OkDesktopCommunityProfileHeader/components/OkDesktopCommunityProfileHeaderBar/components/OkDesktopCommunityProfileHeaderBarIds/index.tsx
@@ -5,8 +5,8 @@ import twi, { tw } from "~/lib/tailwind";
 export default function OkDesktopCommunityProfileHeaderBarIds() {
     return (
         <View style={tw`mx-2`}>
-            <Text style={tw`font-bold text-black dark:text-white`}>Coffe</Text>
-            <Text style={tw`font-bold text-black dark:text-white`}>c/coffee</Text>
+            <Text style={tw`font-bold text-gray-500 dark:text-white`}>Coffe</Text>
+            <Text style={tw`font-medium text-gray-500 dark:text-white`}>c/coffee</Text>
         </View>
     )
 }
