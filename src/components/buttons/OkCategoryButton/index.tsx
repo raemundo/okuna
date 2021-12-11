d8d1367f7ead4c28f99022967f934208596d33a7
raemundo <eimooamai@outlook.com>
2021-12-12 00:08:00 +0200

    fix(OkCategoryButton): style

--- src/components/buttons/OkCategoryButton/index.tsx
+++ src/components/buttons/OkCategoryButton/index.tsx
@@ -4,6 +4,6 @@ import twi, { tw } from "~/lib/tailwind";
 
 export default function OkCategoryButton({ children, color}) {
     return (
-        <Pressable style={(state) => [tw`px-2 py-1 mx-1 bg-${color}-500 rounded-xl`]}><Text style={tw`text-white`}>{children}</Text></Pressable>
+        <Pressable style={(state) => [tw`px-2 py-1 mx-1 bg-${color}-500 rounded-lg`]}><Text>{children}</Text></Pressable>
     )
 }
