20e142915c88d811c3d5d297b7d70dedd653f981
raemundo <eimooamai@outlook.com>
2021-12-11 21:37:29 +0200

    fix(OkCommunityProfileInfoButtons): style

--- src/screens/OkCommunityProfile/components/shared/OkCommunityProfileInfoButtons/index.tsx
+++ src/screens/OkCommunityProfile/components/shared/OkCommunityProfileInfoButtons/index.tsx
@@ -10,8 +10,8 @@ export default function OkCommunityProfileInfoButtons() {
     const { t } = useTranslation()
     return (
         <View style={tw`flex-row`}>
-            <View style={tw`flex-row items-center p-1 m-2 bg-gray-200 rounded-lg`}><Text style={tw`text-gray-600`}><FontAwesomeIcon style={twi`text-gray-600`} icon={faSmile} /> {t('community__button_staff')}</Text></View>
-            <View style={tw`flex-row items-center p-1 m-2 bg-gray-200 rounded-lg`}><Text style={tw`text-gray-600`}><FontAwesomeIcon style={twi`text-gray-600`} icon={faBook} /> {t('community__button_rules')}</Text></View>
+            <View style={tw`flex-row items-center p-1 m-1 bg-gray-200 rounded-lg`}><Text style={tw`text-gray-600`}><FontAwesomeIcon style={twi`text-gray-600`} icon={faSmile} /> {t('community__button_staff')}</Text></View>
+            <View style={tw`flex-row items-center p-1 m-1 bg-gray-200 rounded-lg`}><Text style={tw`text-gray-600`}><FontAwesomeIcon style={twi`text-gray-600`} icon={faBook} /> {t('community__button_rules')}</Text></View>
         </View>
     )
 }
