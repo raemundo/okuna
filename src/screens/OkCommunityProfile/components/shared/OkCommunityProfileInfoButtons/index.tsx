a8b619b95eaad56a7d635cbb4bcdac9a1ae2a0ea
raemundo <eimooamai@outlook.com>
2021-12-11 23:50:05 +0200

    fix(OkCommunityProfileInfoButtons): style

--- src/screens/OkCommunityProfile/components/shared/OkCommunityProfileInfoButtons/index.tsx
+++ src/screens/OkCommunityProfile/components/shared/OkCommunityProfileInfoButtons/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Pressable } from 'react-native'
 import twi, { tw } from "~/lib/tailwind";
 import FontAwesomeIcon from "~/component-lib/FontAwesomeIcon";
 import { faSmile, faBook } from "@fortawesome/free-solid-svg-icons";
@@ -9,9 +9,13 @@ import { useTranslation } from 'react-i18next';
 export default function OkCommunityProfileInfoButtons() {
     const { t } = useTranslation()
     return (
-        <View style={tw`flex-row`}>
-            <View style={tw`flex-row items-center p-1 m-1 bg-gray-200 rounded-lg`}><Text style={tw`text-gray-600`}><FontAwesomeIcon style={twi`text-gray-600`} icon={faSmile} /> {t('community__button_staff')}</Text></View>
-            <View style={tw`flex-row items-center p-1 m-1 bg-gray-200 rounded-lg`}><Text style={tw`text-gray-600`}><FontAwesomeIcon style={twi`text-gray-600`} icon={faBook} /> {t('community__button_rules')}</Text></View>
+        <View style={tw`flex-row my-2`}>
+            <Pressable style={(state) => [tw`flex-row items-center py-1 px-3 m-1 bg-gray-200 rounded-xl`]}>
+                <Text style={tw`text-gray-600`}><FontAwesomeIcon style={twi`text-gray-500`} icon={faSmile} /> {t('community__button_staff')}</Text>
+            </Pressable>
+            <Pressable style={(state) => [tw`flex-row items-center py-1 px-3 m-1 bg-gray-200 rounded-xl`]}>
+                <Text style={tw`text-gray-600`}><FontAwesomeIcon style={twi`text-gray-500`} icon={faBook} /> {t('community__button_rules')}</Text>
+            </Pressable>
         </View>
     )
 }
