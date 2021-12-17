e039372a626ce4be79e38dd27c0535b4fd08b350
raemundo <eimooamai@outlook.com>
2021-12-17 21:28:57 +0200

    feat(OkDesktopCommunitiesCategory): reinit

--- src/screens/OkCommunitiesPage/components/OkDesktopCommunitiesPage/components/OkDesktopCommunitiesCategory/index.tsx
+++ src/screens/OkCommunitiesPage/components/OkDesktopCommunitiesPage/components/OkDesktopCommunitiesCategory/index.tsx
@@ -2,10 +2,16 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import twi, { tw } from "~/lib/tailwind";
 
+import OkCategoryPreviewButton from "~/components/buttons/OkCategoryPreviewButton";
+import OkFatButton from "~/components/buttons/OkFatButton";
+import OkUserAvatar from "~/components/avatars/OkUserAvatar";
+import OkHttpList from "~/components/OkHttpList";
+import OkCommunityCard from '~/components/cards/OkCommunityCard';
+
 export default function OkDesktopCommunitiesCategory() {
     return (
-        <View>
-            <Text></Text>
-        </View>
+        <OkHttpList listType="community">
+            <OkCommunityCard />
+        </OkHttpList>
     )
 }
