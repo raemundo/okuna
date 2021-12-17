d0d0036b75a253dfd5e25635528ea41aef12f3f5
raemundo <eimooamai@outlook.com>
2021-12-17 16:15:48 +0200

    feat(OkDesktopUserProfileSidebar): init

---
+++ src/screens/OkUserProfilePage/components/OkDesktopUserProfilePage/components/OkDesktopUserProfileSidebar/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import twi, { tw } from "~/lib/tailwind";
+
+export default function OkDesktopUserProfileSidebar() {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
