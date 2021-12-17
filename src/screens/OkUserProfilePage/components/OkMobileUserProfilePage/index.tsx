258009ebebd9223f8f673886b8d5e1c7bda39182
raemundo <eimooamai@outlook.com>
2021-12-17 20:00:51 +0200

    feat(OkMobileUserProfilePage): reinit

--- src/screens/OkUserProfilePage/components/OkMobileUserProfilePage/index.tsx
+++ src/screens/OkUserProfilePage/components/OkMobileUserProfilePage/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 import twi, { tw } from "~/lib/tailwind";
+import OkUserCover from "~/components/covers/OkUserCover";
+import OkMobileUserProfileHeader from "./components/OkMobileUserProfileHeader";
+import OkMobileUserProfileCard from "./components/OkMobileUserProfileCard";
+import OkUserPostsStream from '~/components/posts-stream/OkUserPostsStream';
 
 export default function OkMobileUserProfilePage() {
     return (
         <View>
-            <Text></Text>
+            <OkUserCover />
+            <OkMobileUserProfileHeader />
+            <OkMobileUserProfileCard />
+            <OkUserPostsStream />
         </View>
     )
 }
