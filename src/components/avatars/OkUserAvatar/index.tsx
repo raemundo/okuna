cca51153ac7db0171af9550999370543174ebc58
raemundo <eimooamai@outlook.com>
2021-12-17 19:01:21 +0200

    feat(OkUserAvatar): reinit

--- src/components/avatars/OkUserAvatar/index.tsx
+++ src/components/avatars/OkUserAvatar/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 import twi, { tw } from "~/lib/tailwind";
+import FontAwesomeIcon from "~/component-lib/FontAwesomeIcon";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import OkImageAvatar from "~/components/avatars/OkImageAvatar";
+import OkLetterAvatar from "~/components/avatars/OkLetterAvatar";
+import Avatar from "~/component-lib/Avatar";
 
 export default function OkUserAvatar() {
     return (
-        <View>
-            <Text></Text>
-        </View>
+        <>
+            <Avatar source={{ uri: 'https://picsum.photos/200' }} style={tw`w-18 h-18`} />
+        </>
     )
 }
