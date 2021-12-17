5643ec77c44a52aaeb19f78d173d1159dd1d5dec
raemundo <eimooamai@outlook.com>
2021-12-17 21:04:03 +0200

    feat(OkFatButton): reinit

--- src/components/buttons/OkFatButton/index.tsx
+++ src/components/buttons/OkFatButton/index.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import twi, { tw } from "~/lib/tailwind";
 
-export default function OkFatButton() {
+export default function OkFatButton({text, children, color="yellow"}) {
     return (
-        <View>
-            <Text></Text>
+        <View style={tw`bg-white rounded-lg`}>
+            {children}
+            <Text style={tw`bg-${color}-500 rounded-b-lg`}>{text}</Text>
         </View>
     )
 }
