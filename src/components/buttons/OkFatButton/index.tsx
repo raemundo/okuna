ba9d234593a59c43a8f24a80ec88863a1ee66b01
raemundo <eimooamai@outlook.com>
2021-12-17 21:09:34 +0200

    feat(OkFatButton): add text color

--- src/components/buttons/OkFatButton/index.tsx
+++ src/components/buttons/OkFatButton/index.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import twi, { tw } from "~/lib/tailwind";
 
-export default function OkFatButton({text, children, color="yellow"}) {
+export default function OkFatButton({text, children, color="yellow-500"}) {
     return (
         <View style={tw`bg-white rounded-lg`}>
             {children}
-            <Text style={tw`bg-${color}-500 rounded-b-lg`}>{text}</Text>
+            <Text style={tw`bg-${color} rounded-b-lg text-white`}>{text}</Text>
         </View>
     )
 }
