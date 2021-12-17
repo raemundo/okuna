2030e9fb656861dfeeb9231c0aea154095e7b6ce
raemundo <eimooamai@outlook.com>
2021-12-17 21:29:48 +0200

    fix(OkFatButton): style

--- src/components/buttons/OkFatButton/index.tsx
+++ src/components/buttons/OkFatButton/index.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import twi, { tw } from "~/lib/tailwind";
 
-export default function OkFatButton({text, children, color="yellow-500"}) {
+export default function OkFatButton({text, children, textColor="yellow-500"}) {
     return (
-        <View style={tw`bg-white rounded-lg`}>
+        <View style={tw`bg-white rounded-lg justify-between`}>
             {children}
-            <Text style={tw`bg-${color} rounded-b-lg text-white`}>{text}</Text>
+            <Text style={tw`bg-${textColor} rounded-b-lg text-white text-center`}>{text}</Text>
         </View>
     )
 }
