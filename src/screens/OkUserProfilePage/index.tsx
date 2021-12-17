b7c155af5dacf212a3d13e665e5db4d282f6eaf5
raemundo <eimooamai@outlook.com>
2021-12-17 16:04:37 +0200

    feat(OkUserProfilePage): init

--- src/screens/OkUserProfilePage/index.tsx
+++ src/screens/OkUserProfilePage/index.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import twi, { tw } from "~/lib/tailwind";
 import OkHomePage from "~/layouts/OkHomePage";
+import OkDesktopUserProfilePage from './components/OkDesktopUserProfilePage/';
+import OkMobileUserProfilePage from './components/OkMobileUserProfilePage/';
 
 export default function OkUserProfilePage() {
     return (
         <OkHomePage>
-            <View>
-                <Text></Text>
-            </View>
+            <OkHomePage>
+                {twi.prefixMatch('md') && <OkDesktopUserProfilePage/>}
+                {!twi.prefixMatch('md') && <OkMobileUserProfilePage/>}
+            </OkHomePage>
         </OkHomePage>
     )
 }
