a4dc1a554ccd113237e37d2cce36b11073dc0594
raemundo <eimooamai@outlook.com>
2021-12-17 20:59:03 +0200

    feat(OkCommunitiesPage): reinit

--- src/screens/OkCommunitiesPage/index.tsx
+++ src/screens/OkCommunitiesPage/index.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import twi, { tw } from "~/lib/tailwind";
 import OkHomePage from "~/layouts/OkHomePage";
+import OkDesktopCommunitiesPage from "./components/OkDesktopCommunitiesPage";
+import OkMobileCommunitiesPage from "./components/OkMobileCommunitiesPage";
+import OkNewCommunityAction from '~/components/actions/OkNewCommunityAction';
 
 export default function OkCommunitiesPage() {
     return (
         <OkHomePage>
-            <View>
-                <Text></Text>
-            </View>
+            {twi.prefixMatch('md') && <OkDesktopCommunitiesPage />}
+            {!twi.prefixMatch('md') && <OkMobileCommunitiesPage />}
         </OkHomePage>
     )
 }
