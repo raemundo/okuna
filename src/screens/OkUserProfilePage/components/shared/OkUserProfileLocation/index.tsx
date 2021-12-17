708d67465391beb07adc97c6e84eb5a5c97f2323
raemundo <eimooamai@outlook.com>
2021-12-17 18:01:58 +0200

    feat(OkUserProfileLocation): reinit

--- src/screens/OkUserProfilePage/components/shared/OkUserProfileLocation/index.tsx
+++ src/screens/OkUserProfilePage/components/shared/OkUserProfileLocation/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 import twi, { tw } from "~/lib/tailwind";
+import FontAwesomeIcon from '~/component-lib/FontAwesomeIcon';
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 
-export default function OkUserProfileLocation() {
+export default function OkUserProfileLocation({ style = {} }) {
     return (
-        <View>
-            <Text></Text>
+        <View style={[tw`flex-row items-center`, style]}>
+            <FontAwesomeIcon icon={faMapMarkerAlt} />
+            <Text style={tw`dark:text-white px-1`}>France</Text>
         </View>
     )
 }
