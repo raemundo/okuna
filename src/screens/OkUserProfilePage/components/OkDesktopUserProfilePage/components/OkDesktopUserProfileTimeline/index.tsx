af70cb4cf37cbc4de4aafc6e74c9d9d7ae71fc97
raemundo <eimooamai@outlook.com>
2021-12-17 16:53:14 +0200

    feat(OkDesktopUserProfileTimeline): init

--- src/screens/OkUserProfilePage/components/OkDesktopUserProfilePage/components/OkDesktopUserProfileTimeline/index.tsx
+++ src/screens/OkUserProfilePage/components/OkDesktopUserProfilePage/components/OkDesktopUserProfileTimeline/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 import twi, { tw } from "~/lib/tailwind";
+import OkUserPostsStream from "~/components/posts-stream/OkUserPostsStream";
 
 export default function OkDesktopUserProfileTimeline() {
     return (
-        <View>
-            <Text></Text>
+        <View style={tw`w-full md:w-4/6 md:pl-2`}>
+            <OkUserPostsStream />
         </View>
     )
 }
