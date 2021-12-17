82cbad40e85c5d5fdf6f9d46f0fd7d7c107af71e
raemundo <eimooamai@outlook.com>
2021-12-17 20:38:00 +0200

    feat(OkMobileUserProfileFollowersCount): reinit

--- src/screens/OkUserProfilePage/components/OkMobileUserProfilePage/components/OkMobileUserProfileCard/components/OkMobileUserProfileFollowersCount/index.tsx
+++ src/screens/OkUserProfilePage/components/OkMobileUserProfilePage/components/OkMobileUserProfileCard/components/OkMobileUserProfileFollowersCount/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next';
 import { View, Text } from 'react-native'
 import twi, { tw } from "~/lib/tailwind";
 
 export default function OkMobileUserProfileFollowersCount() {
+    const {t} = useTranslation()
     return (
-        <View>
-            <Text></Text>
-        </View>
+        <Text style={tw`dark:text-white`}><Text>12</Text>{t('post__profile_counts_followers')}</Text>
     )
 }
