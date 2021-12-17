93f2c77a50bff2c8cc71e36e8950d059f71c4c96
raemundo <eimooamai@outlook.com>
2021-12-17 16:50:48 +0200

    feat(OkDesktopUserProfilePage): init

--- src/screens/OkUserProfilePage/components/OkDesktopUserProfilePage/index.tsx
+++ src/screens/OkUserProfilePage/components/OkDesktopUserProfilePage/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 import twi, { tw } from "~/lib/tailwind";
+import OkDesktopUserProfileHeader from "./components/OkDesktopUserProfileHeader"
+import OkDesktopUserProfileSidebar from "./components/OkDesktopUserProfileSidebar";
+import OkDesktopUserProfileTimeline from "./components/OkDesktopUserProfileTimeline"
+
 
 export default function OkDesktopUserProfilePage() {
     return (
-        <View>
-            <Text></Text>
+        <View style={tw`w-9/12 h-full mx-auto mt-6`}>
+            <OkDesktopUserProfileHeader />
+            <View style={tw`flex-row flex-wrap h-full mt-2`}>
+                <OkDesktopUserProfileSidebar />
+                <OkDesktopUserProfileTimeline />
+            </View>
         </View>
     )
 }
