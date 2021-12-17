5cd914b94ca83b0b55d956aaccb3476f017b140e
raemundo <eimooamai@outlook.com>
2021-12-17 20:28:04 +0200

    feat(OkUserProfileBio): reinit

--- src/screens/OkUserProfilePage/components/shared/OkUserProfileBio/index.tsx
+++ src/screens/OkUserProfilePage/components/shared/OkUserProfileBio/index.tsx
@@ -1,11 +1,12 @@
+import OkSmartText from '~/components/smart-text/OkSmartText';
 import React from 'react'
 import { View, Text } from 'react-native'
 import twi, { tw } from "~/lib/tailwind";
 
 export default function OkUserProfileBio() {
     return (
-        <View>
-            <Text></Text>
+        <View style={tw`p-2`}>
+            <OkSmartText text="Digital right activist building people-centric responsible, sustainable and fair technology for a more prosperous tomorrow" />
         </View>
     )
 }
