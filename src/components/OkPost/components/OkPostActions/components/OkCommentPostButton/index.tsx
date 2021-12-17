f2ff1f19d0b8eb7adba115ae5f4e5df1afe263fd
raemundo <eimooamai@outlook.com>
2021-12-17 15:54:33 +0200

    feat(OkCommentPostButton): change icon

--- src/components/OkPost/components/OkPostActions/components/OkCommentPostButton/index.tsx
+++ src/components/OkPost/components/OkPostActions/components/OkCommentPostButton/index.tsx
@@ -1,4 +1,4 @@
-import { faComment } from '@fortawesome/free-solid-svg-icons';
+import { faCommentAlt } from '@fortawesome/free-solid-svg-icons';
 import FontAwesomeIcon from "~/component-lib/FontAwesomeIcon";
 import React from 'react'
 import { useTranslation } from 'react-i18next';
@@ -9,7 +9,7 @@ export default function OkCommentPostButton() {
     const { t } = useTranslation()
     return (
         <Pressable style={tw`flex-row items-center justify-center w-5/12 p-2 bg-gray-200 dark:bg-gray-900 rounded-full`}>
-            <FontAwesomeIcon icon={faComment} style={twi`dark:text-white`} />
+            <FontAwesomeIcon icon={faCommentAlt} style={twi`dark:text-white`} />
             <Text style={tw`mx-1 text-sm font-semibold dark:text-white`}>{t('post__action_comment')}</Text>
         </Pressable>
     )
