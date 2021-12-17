084c8053a1ed27c12d51c2743a6842f6382ee702
raemundo <eimooamai@outlook.com>
2021-12-17 20:38:30 +0200

    feat(OkMobileUserProfilePostsCount): reinit

--- src/screens/OkUserProfilePage/components/OkMobileUserProfilePage/components/OkMobileUserProfileCard/components/OkMobileUserProfilePostsCount/index.tsx
+++ src/screens/OkUserProfilePage/components/OkMobileUserProfilePage/components/OkMobileUserProfileCard/components/OkMobileUserProfilePostsCount/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next';
 import { View, Text } from 'react-native'
 import twi, { tw } from "~/lib/tailwind";
 
 export default function OkMobileUserProfilePostsCount() {
+    const { t } = useTranslation()
     return (
-        <View>
-            <Text></Text>
-        </View>
+        <Text style={tw`dark:text-white`}><Text>1.2K</Text>{t('post__profile_counts_posts')}</Text>
     )
 }
