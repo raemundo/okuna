114501e8ce6b7497fa6865c024427fd2683f4b8c
raemundo <eimooamai@outlook.com>
2021-12-17 19:10:29 +0200

    feat(OkDesktopUserProfileHeaderBarStats): init

--- src/screens/OkUserProfilePage/components/OkDesktopUserProfilePage/components/OkDesktopUserProfileHeader/components/OkDesktopUserProfileHeaderBar/components/OkDesktopUserProfileHeaderBarStats/index.tsx
+++ src/screens/OkUserProfilePage/components/OkDesktopUserProfilePage/components/OkDesktopUserProfileHeader/components/OkDesktopUserProfileHeaderBar/components/OkDesktopUserProfileHeaderBarStats/index.tsx
@@ -4,8 +4,15 @@ import twi, { tw } from "~/lib/tailwind";
 
 export default function OkDesktopUserProfileHeaderBarStats() {
     return (
-        <View>
-            <Text></Text>
-        </View>
+        <>
+            <View style={tw`hidden mx-2 md:flex`}>
+                <Text style={tw`font-bold text-black dark:text-white`}>Post</Text>
+                <Text style={tw`font-bold text-black dark:text-white`}>1.2K</Text>
+            </View>
+            <View style={tw`hidden mx-2 md:flex`}>
+                <Text style={tw`font-bold text-black dark:text-white`}>Following</Text>
+                <Text style={tw`font-bold text-black dark:text-white`}>20K</Text>
+            </View>
+        </>
     )
 }
