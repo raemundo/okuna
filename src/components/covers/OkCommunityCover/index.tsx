fcb191d9ae2cdd78e8c4713871b55ed02a4c6872
raemundo <eimooamai@outlook.com>
2021-12-20 00:05:13 +0200

    feat(OkCommunityCover): add style prop

--- src/components/covers/OkCommunityCover/index.tsx
+++ src/components/covers/OkCommunityCover/index.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { ImageBackground } from 'react-native'
 import twi, { tw } from "~/lib/tailwind";
 
-export default function OkCommunityCover() {
+export default function OkCommunityCover({style={}}) {
     return (
         <ImageBackground
             source={{ uri: "https://picsum.photos/720" }}
             imageStyle={tw`rounded-t-2`}
-            style={tw`h-96 w-full rounded-t-4`}
+            style={[tw`w-full h-96 rounded-t-4`, style]}
         ></ImageBackground>
     )
 }
