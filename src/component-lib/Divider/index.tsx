d436d88b0819ee1565947c477e79566270905d1a
raemundo <eimooamai@outlook.com>
2021-12-19 23:54:45 +0200

    refactor: Divider

--- src/component-lib/Divider/index.tsx
+++ src/component-lib/Divider/index.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import twi, { tw } from "~/lib/tailwind";
 
-export default function Divider() {
+export default function Divider({style={}}) {
     return (
-        <View style={tw`flex-row mt-5 border-t border-gray-400 dark:border-gray-900`}></View>
+        <View style={[tw`flex-row border-t border-gray-400 dark:border-gray-900`, style]}></View>
     )
 }
