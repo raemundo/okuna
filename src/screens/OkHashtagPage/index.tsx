9a9822c4827a6bf0add72b246243ef42d4f52a80
raemundo <eimooamai@outlook.com>
2021-12-24 00:17:07 +0200

    feat(OkHashtagPage): reinit

--- src/screens/OkHashtagPage/index.tsx
+++ src/screens/OkHashtagPage/index.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import twi, { tw } from "~/lib/tailwind";
 import OkHomePage from "~/layouts/OkHomePage";
+import OkMobileHeader from "~/components/mobile-only/OkMobileHeader";
+import OkHashtagBanner from "~/components/tiles/OkHashtagBanner";
+import OkHashtagPostsStream from "~/components/posts-stream/OkHashtagPostsStream";
 
 export default function OkHashtagPage() {
     return (
         <OkHomePage>
-            <View>
-                <Text></Text>
+            <View style={tw`w-full mx-auto mt-5 lg:w-6/12`}>
+                <OkHashtagPostsStream />
             </View>
         </OkHomePage>
     )
