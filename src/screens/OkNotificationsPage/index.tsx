37c2afa216f3907dc47fd3137d2f8e097eb1efd9
raemundo <eimooamai@outlook.com>
2021-12-24 00:07:58 +0200

    feat(OkNotificationsPage): reinit

--- src/screens/OkNotificationsPage/index.tsx
+++ src/screens/OkNotificationsPage/index.tsx
@@ -2,13 +2,12 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import twi, { tw } from "~/lib/tailwind";
 import OkHomePage from "~/layouts/OkHomePage";
-
+import OkMobileHeader from "~/components/mobile-only/OkMobileHeader";
+import OkUserNotifications from "~/components/OkUserNotifications";
 export default function OkNotificationsPage() {
     return (
         <OkHomePage>
-            <View>
-                <Text></Text>
-            </View>
+            <OkUserNotifications />
         </OkHomePage>
     )
 }
