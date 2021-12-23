d1a4bfde8fd7a321e9aad03eb7fc7925fb4bb178
raemundo <eimooamai@outlook.com>
2021-12-24 00:16:52 +0200

    feat(OkHashtagPostsStream): reinit

--- src/components/posts-stream/OkHashtagPostsStream/index.tsx
+++ src/components/posts-stream/OkHashtagPostsStream/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 import twi, { tw } from "~/lib/tailwind";
-
+import OkPostsStream from '../OkPostsStream';
 export default function OkHashtagPostsStream() {
     return (
-        <View>
-            <Text></Text>
-        </View>
+        <OkPostsStream />
     )
 }
