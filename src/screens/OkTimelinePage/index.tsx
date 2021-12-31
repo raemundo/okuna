4d939dbadc539508fd6cfd2cd8502304259d33a2
raemundo <eimooamai@outlook.com>
2021-12-31 12:15:27 +0200

    feat(OkTimelinePage): reinit

--- src/screens/OkTimelinePage/index.tsx
+++ src/screens/OkTimelinePage/index.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { View, Text } from "react-native";
 import twi, { tw } from "~/lib/tailwind";
 import OkHomePage from "~/layouts/OkHomePage";
+import OkPostsStream from "~/components/posts-stream/OkPostsStream";
+import OkTimelinePostUploads from "./components/OkTimelinePostUploads";
+import OkNewPostAction from "~/components/OkNewPostAction";
 
 export default function OkTimelinePage() {
-    return (
-        <OkHomePage>
-            <View>
-                <Text></Text>
-            </View>
-        </OkHomePage>
-    )
+  return (
+    <OkHomePage>
+      <OkPostsStream leading={<OkTimelinePostUploads />} />
+      <OkNewPostAction />
+    </OkHomePage>
+  );
 }
