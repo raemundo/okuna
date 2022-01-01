40638cf1fa2e423607b2cae96159c1db97e2b8b1
raemundo <eimooamai@outlook.com>
2022-01-01 18:20:27 +0200

    feat(OkResetPasswordPage): reinit

--- src/components/forms/OkResetPasswordForm/index.tsx
+++ src/components/forms/OkResetPasswordForm/index.tsx
@@ -1,11 +1,50 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from "react";
+import { View, Text, TextInput } from "react-native";
 import twi, { tw } from "~/lib/tailwind";
+import OkButtonsNavigation from "~/components/navigation/OkButtonsNavigation";
+import { useTranslation } from "react-i18next";
 
 export default function OkResetPasswordForm() {
-    return (
-        <View>
-            <Text></Text>
-        </View>
-    )
+  const { t } = useTranslation();
+  const [activeStep, setActiveStep] = useState(0);
+  const goToPreviousStep = () => {
+    setActiveStep(activeStep !== 0 ? activeStep - 1 : activeStep);
+  };
+  const goToNextStep = () => {
+    setActiveStep(activeStep !== 1 ? activeStep + 1 : activeStep);
+  };
+  return (
+    <>
+      {activeStep === 0 && (
+        <>
+          <Text style={tw`dark:text-white text-center text-xl font-bold`}>
+            {t("auth__change_password_title")}
+          </Text>
+          <Text style={tw`dark:text-white`}>
+            {t("auth__change_password_new_pwd")}
+          </Text>
+          <Text style={tw`dark:text-white`}>
+            {t("auth__change_password_new_pwd_hint")}
+          </Text>
+          <TextInput
+            placeholder={t("auth__create_acc__name_placeholder")}
+            style={tw`bg-gray-100 p-2 rounded-3xl text-l`}
+          />
+          <Text style={tw`text-red-500`}>
+            {t("auth__change_password_new_pwd_error")}
+          </Text>
+          <Text style={tw``}>{t("auth__change_password_save_text")}</Text>
+        </>
+      )}
+      {activeStep === 1 && (
+        <>
+          <Text style={tw``}>{t("auth__change_password_save_success")}</Text>
+        </>
+      )}
+      <OkButtonsNavigation
+        onPrevious={goToPreviousStep}
+        onNext={goToNextStep}
+      />
+    </>
+  );
 }
--- src/screens/OkResetPasswordPage/index.tsx
+++ src/screens/OkResetPasswordPage/index.tsx
@@ -2,13 +2,18 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import twi, { tw } from "~/lib/tailwind";
 import OkAuthPage from "~/layouts/OkAuthPage";
+import Card from '~/component-lib/Card';
+import OkLogo from '~/components/OkLogo';
+import OkRequestPasswordResetForm from '~/components/forms/OkRequestPasswordResetForm';
+import OkResetPasswordForm from '~/components/forms/OkResetPasswordForm';
 
 export default function OkResetPasswordPage() {
     return (
         <OkAuthPage>
-            <View>
-                <Text></Text>
-            </View>
+            <Card style={tw`rounded-lg md:w-1/2 p-2`}>
+                <OkLogo style={tw`justify-center`} />
+                <OkResetPasswordForm />
+            </Card>
         </OkAuthPage>
     )
 }
