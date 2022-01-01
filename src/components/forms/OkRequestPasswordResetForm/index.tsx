b6146f54283557234d4d8a7af303d865f12cca89
raemundo <eimooamai@outlook.com>
2022-01-01 17:58:48 +0200

    feat(OkRequestPasswordResetPage): reinit

--- src/components/forms/OkRequestPasswordResetForm/index.tsx
+++ src/components/forms/OkRequestPasswordResetForm/index.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { View, Text, TextInput } from "react-native";
 import twi, { tw } from "~/lib/tailwind";
+import Twemoji from "~/component-lib/Twemoji";
+import OkButtonsNavigation from "~/components/navigation/OkButtonsNavigation";
 
 export default function OkRequestPasswordResetForm() {
-    return (
-        <View>
-            <Text></Text>
-        </View>
-    )
+  const { t } = useTranslation();
+  return (
+    <>
+      <Text style={tw`text-lg font-bold dark:text-gray-500`}>
+        {t("auth__create_acc__what_email")}
+      </Text>
+      <TextInput
+        style={tw`bg-gray-100 p-2 rounded-3xl text-lg`}
+        placeholder={t("auth__create_acc__email_placeholder")}
+      />
+      <Text style={tw`text-red-500`}>
+        {t("auth__create_acc__email_empty_error")}
+      </Text>
+      <Text style={tw`text-red-500`}>
+        {t("auth__create_acc__email_invalid_error")}
+      </Text>
+      <Twemoji>📫</Twemoji>
+      <OkButtonsNavigation />
+      <Text>{t("auth__reset_password_success_info")}</Text>
+    </>
+  );
 }
--- src/screens/OkRequestPasswordResetPage/index.tsx
+++ src/screens/OkRequestPasswordResetPage/index.tsx
@@ -2,13 +2,17 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import twi, { tw } from "~/lib/tailwind";
 import OkAuthPage from "~/layouts/OkAuthPage";
+import Card from '~/component-lib/Card';
+import OkLogo from "~/components/OkLogo";
+import OkRequestPasswordResetForm from "~/components/forms/OkRequestPasswordResetForm";
 
 export default function OkRequestPasswordResetPage() {
     return (
         <OkAuthPage>
-            <View>
-                <Text></Text>
-            </View>
+            <Card style={tw`rounded-lg md:w-1/2`}>
+                <OkLogo style={tw`justify-center`} />
+                <OkRequestPasswordResetForm />
+            </Card>
         </OkAuthPage>
     )
 }
