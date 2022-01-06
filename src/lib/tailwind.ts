f2e9b931ef8ee99a19835cf19728deac4b7ea55e
raemundo <eimooamai@outlook.com>
2022-01-06 21:15:00 +0200

    feat(lib/tailwind): add useDeviceContext

--- src/lib/tailwind.ts
+++ src/lib/tailwind.ts
@@ -1,22 +1,25 @@
-import { create, useDeviceContext, useAppColorScheme } from 'twrnc';
+import { create, useAppColorScheme } from "twrnc";
 import { useState, useEffect } from "react";
-import { StyleSheet } from 'react-native';
+import { StyleSheet, useColorScheme, useWindowDimensions } from "react-native";
 
 // create the customized version...
 const twi = create(require(`../../tailwind.config.js`)); // <- your path may differ
 
-export const tw = (strings: TemplateStringsArray, ...values: (string | number)[]) => {
-    let str = ``;
-    strings.forEach((string, i) => {
-        str += string + (values[i] || ``);
-    });
-    return StyleSheet.create({ x: twi`${str}` }).x;
+export const tw = (
+  strings: TemplateStringsArray,
+  ...values: (string | number)[]
+) => {
+  let str = ``;
+  strings.forEach((string, i) => {
+    str += string + (values[i] || ``);
+  });
+  return StyleSheet.create({ x: twi`${str}` }).x;
 };
 
 tw.style = (...exprs) => StyleSheet.create({ x: twi.style(...exprs) }).x;
 // ... and then this becomes the main function your app uses
 export default twi;
-
+/*
 export function useDeviceContextSever(tw) {
     const [isClient, setIsClient] = useState(false);
     useEffect(() => {
@@ -24,4 +27,28 @@ export function useDeviceContextSever(tw) {
     }, []);
 
     useDeviceContext(isClient ? tw : tw, { withDeviceColorScheme: false });
+}
+*/
+
+export function useDeviceContext(tw, opts = { withDeviceColorScheme: true }) {
+  const [isClient, setIsClient] = useState(false);
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+  const window = useWindowDimensions();
+  const win = isClient
+    ? window
+    : {
+        fontScale: 1,
+        height: 218,
+        scale: 1,
+        width: 1349,
+      };
+  tw.setWindowDimensions(win);
+  tw.setFontScale(win.fontScale);
+  tw.setPixelDensity(win.scale === 1 ? 1 : 2);
+  const colorScheme = useColorScheme();
+  if (opts.withDeviceColorScheme) {
+    tw.setColorScheme(colorScheme);
+  }
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,19 +2,21 @@ import React, { useState, useEffect } from "react";
 import App from "next/app";
 import { useRouter } from "next/router";
 import "~/assets/styles/index.scss"
-import tw, { useDeviceContextSever } from "~/lib/tailwind";
+import tw, { useDeviceContext } from "~/lib/tailwind";
 import { useAppColorScheme } from "twrnc";
 import "~/lib/i18n";
 import { TouchableOpacity, Text } from "react-native";
 
 function MyApp({ Component, pageProps }) {
-  useDeviceContextSever(tw)
+  useDeviceContext(tw)
   const [colorScheme, toggleColorScheme, setColorScheme] = useAppColorScheme(tw);
   tw.setColorScheme(colorScheme);
   const router = useRouter();
   return (
     <>
-      <TouchableOpacity onPress={toggleColorScheme}>
+      <TouchableOpacity
+       onPress={toggleColorScheme}
+      >
         <Text style={tw`bg-red-500 text-black dark:text-white`}>Switch Color Scheme {colorScheme} </Text>
       </TouchableOpacity>
       <Component {...pageProps} key={router.asPath} />
