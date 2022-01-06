f8b6387af1c72adf50c1a6a43f45cbbe6955994b
raemundo <eimooamai@outlook.com>
2022-01-06 22:09:46 +0200

    fix(OkPostReactions): div soup

--- src/components/OkPost/components/OkPostReactions/index.tsx
+++ src/components/OkPost/components/OkPostReactions/index.tsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import { View } from 'react-native'
+import React from "react";
+import { View } from "react-native";
 import { tw } from "~/lib/tailwind";
 import OkEmojiReactionButton from "~/components/buttons/OkEmojiReactionButton";
 
 export default function OkPostReactions() {
-    return (
-        <View style={tw`flex-row items-center justify-between mx-2 my-2`}>
-            <View style={tw`flex-row items-center text-sm`}>
-                {[{ count: 15, emoji: '❤' }, { count: 10, emoji: '👍' }, { count: 11, emoji: '😁' }].map((item, idx) => (
-                    <OkEmojiReactionButton key={idx} item={item} />
-                ))}
-            </View>
-        </View>
-    )
+  return (
+    <View style={tw`flex-row items-center text-sm mx-2 my-2`}>
+      {[
+        { count: 15, emoji: "❤" },
+        { count: 10, emoji: "👍" },
+        { count: 11, emoji: "😁" },
+      ].map((item, idx) => (
+        <OkEmojiReactionButton key={idx} item={item} />
+      ))}
+    </View>
+  );
 }
